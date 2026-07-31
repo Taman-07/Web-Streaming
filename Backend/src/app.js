200d7faa1020e41f7b274b36e44cec810ddde046
Taman-07 <tamanjot707@gmail.com>
2026-07-31 14:49:52 +0530

    server

---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use("/", (req, res)=>{
+    res.send("Hello from the Server");
+});
+
+
+app.listen(7777, () => {
+    console.log("Server is running on port 7777");
+});
+
