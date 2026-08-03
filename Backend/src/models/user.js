1fa997ceab3fad2f00159253347557da78f64259
Taman-07 <tamanjot707@gmail.com>
2026-08-03 14:00:02 +0530

    indexing

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -4,6 +4,7 @@ const validator=require("validator");
 const userSchema=mongoose.Schema({
     username:{
         type: String,
+        index: true,
         required: true,
         lowercase: true,
         minLength: 5,
